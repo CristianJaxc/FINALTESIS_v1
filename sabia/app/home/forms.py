aa34477ebefd9385a70ede44a6a8a33f7546af52
CristianJaxc <cristian.acosta4503@utc.edu.ec>
2022-06-11 01:36:06 -0500

    commit 5 10/06/2022

--- sabia/app/home/forms.py
+++ sabia/app/home/forms.py
@@ -6,10 +6,10 @@ class ContactosForm(forms.ModelForm):
     nombres = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}),label='Nombres Completos')
     apellidos = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}),label='Apellidos completos')
     email= forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class': 'form-control'}),label='Email')
-    telefono = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}),label='Telefono')
+    telefono = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}),label='Teléfono')
     #direccion = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}),label='Direccion')
     #estado = forms.BooleanField(required=False,label='Estado de la denuncia')
-    descripcion = forms.CharField(required=True,widget=forms.Textarea(attrs={'class': 'form-control'}),label='Descripccion')
+    descripcion = forms.CharField(required=True,widget=forms.Textarea(attrs={'class': 'form-control'}),label='Descripción')
     class Meta:
         model = Contactos
         fields = ['nombres', 'apellidos','email','telefono','descripcion']
